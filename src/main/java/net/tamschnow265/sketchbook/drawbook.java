f0c9c6f05d0becbee45fdd997358d00ac0ecf0f6
TamSchnow265 <TamSchnow265@proton.me>
2022-08-23 19:04:28 +0200

    new icon and more info

--- src/main/java/net/tamschnow265/sketchbook/drawbook.java
+++ src/main/java/net/tamschnow265/sketchbook/drawbook.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class drawbook implements ModInitializer {
-	public static final String MOD_ID ="orderofthetitans";
+	public static final String MOD_ID ="sketchbook";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -18,5 +18,9 @@ public class drawbook implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Opening Sketchbook...");
+
+
+
+		LOGGER.info("Blueprints loaded!");
 	}
 }
