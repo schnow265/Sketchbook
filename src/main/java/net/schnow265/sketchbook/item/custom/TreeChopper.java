e7b12ef9b55b731edf3bcda651bc75b6398aaea8
schnow265 <tamschnow265@proton.me>
2023-05-18 15:26:33 +0200

    Bugfixes: * Choppy now supports Mangrove Logs.
    Development paused for a little time - big feature comming!

--- src/main/java/net/schnow265/sketchbook/item/custom/TreeChopper.java
+++ src/main/java/net/schnow265/sketchbook/item/custom/TreeChopper.java
@@ -29,10 +29,10 @@ public class TreeChopper extends AxeItem {
         if (player != null && !player.isSneaking()) {
             Block block = context.getWorld().getBlockState(context.getBlockPos()).getBlock();
             if (block.equals(Blocks.OAK_LOG) || block.equals(Blocks.SPRUCE_LOG) || block.equals(Blocks.BIRCH_LOG) ||
-                    block.equals(Blocks.JUNGLE_LOG) || block.equals(Blocks.ACACIA_LOG) || block.equals(Blocks.DARK_OAK_LOG)) {
+                    block.equals(Blocks.JUNGLE_LOG) || block.equals(Blocks.ACACIA_LOG) || block.equals(Blocks.DARK_OAK_LOG) || block.equals(Blocks.MANGROVE_LOG)) {
                 // The block is a log block
                 context.getWorld().breakBlock(context.getBlockPos(), true, player);
-                int radius = 1; // the radius of the tree
+                int radius = 5; // the radius of the tree
                 for (int x = -radius; x <= radius; x++) {
                     for (int y = -radius; y <= radius; y++) {
                         for (int z = -radius; z <= radius; z++) {
