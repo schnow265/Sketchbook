4a3cff8e8cd11f910efdebf73ca063b0c9a680c7
schnow265 <tamschnow265@proton.me>
2023-06-06 15:23:41 +0200

    fixed some things

--- src/main/java/net/schnow265/sketchbook/item/ModItems.java
+++ src/main/java/net/schnow265/sketchbook/item/ModItems.java
@@ -5,13 +5,13 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.*;
+import net.minecraft.item.AxeItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.item.ShovelItem;
 
 import net.schnow265.sketchbook.drawbook;
-import net.schnow265.sketchbook.item.custom.frameworks.ModAxeItem;
 import net.schnow265.sketchbook.item.custom.TreeChopper;
 
 public class ModItems {
@@ -34,7 +34,7 @@ public class ModItems {
     public static final Item OP_HAMMER = registerItem("op_hammer", new SwordItem(ToolMaterials.NETHERITE, 999999999,1f, new FabricItemSettings()));
     
     //Axes
-    public static final Item COPPER_C_AXE = registerItem("copper_c_axe", new ModAxeItem(ToolMaterials.IRON, 3, 1f, new FabricItemSettings()));
+    public static final Item COPPER_C_AXE = registerItem("copper_c_axe", new AxeItem(ToolMaterials.IRON, 3, 1f, new FabricItemSettings()));
     public static final Item CHOPPY = registerItem("choppy", new TreeChopper(ToolMaterials.NETHERITE, 0, 1f, new FabricItemSettings()));
     public static final Item IA_SHOVEL = registerItem("ia_shovel", new ShovelItem(ToolMaterials.IRON, 0, (int) 1.15f, new FabricItemSettings()));
 
@@ -42,6 +42,7 @@ public class ModItems {
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(drawbook.MOD_ID, name), item);
     }
+
     // Into the Inventory
     public static void addItemsToItemGroup() {
 
