91e023f53817d107f4438a4a1d74524656a348ae
schnow265 <tamschnow265@proton.me>
2023-07-18 19:31:46 +0200

    Temporary Fix - Items are being added to vanilla Groups.
    Please help me.

--- src/main/java/at/schnow265/sketchbook/item/ModItemGroup.java
+++ src/main/java/at/schnow265/sketchbook/item/ModItemGroup.java
@@ -2,48 +2,33 @@ package at.schnow265.sketchbook.item;
 
 import at.schnow265.sketchbook.drawbook;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
-    public static final ItemGroup SKBITEMS = Registry.register(Registries.ITEM_GROUP, new Identifier(drawbook.MOD_ID, "skbitems"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.skbitems"))
-                    .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
-                        //entries.add(ModItems.);
-                        entries.add(ModItems.RUBY);
-                    }).build());
-    public static final ItemGroup SKCOMABT = Registry.register(Registries.ITEM_GROUP, new Identifier(drawbook.MOD_ID, "skcombat"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.skcombat"))
-                    .icon(() -> new ItemStack(ModItems.N_HAMMER)).entries((displayContext, entries) -> {
-                        //entries.add(ModItems.);
-                        entries.add(ModItems.N_HAMMER);
-                        entries.add(ModItems.A_KNUCKLES);
-                        entries.add(ModItems.A_KNUCKLES);
-                        entries.add(ModItems.COPPER_C_AXE);
-                        entries.add(ModItems.RUBY_KATANA);
-                    }).build());
-    public static final ItemGroup SFUN = Registry.register(Registries.ITEM_GROUP, new Identifier(drawbook.MOD_ID, "sfun"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.sfun"))
-                    .icon(() -> new ItemStack(ModItems.OP_HAMMER)).entries((displayContext, entries) -> {
-                        //entries.add(ModItems.);
-                        entries.add(ModItems.OP_HAMMER);
-                        entries.add(ModItems.IA_SHOVEL);
-                    }).build());
-    public static final ItemGroup SUTILS = Registry.register(Registries.ITEM_GROUP, new Identifier(drawbook.MOD_ID, "sutils"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.sutils"))
-                    .icon(() -> new ItemStack(ModItems.CHOPPY)).entries((displayContext, entries) -> {
-                        //entries.add(ModItems.);
-                        entries.add(ModItems.CHOPPY);
-                    }).build());
+    public static void ItemAdder() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
+            content.add(ModItems.A_KNUCKLES);
+            content.add(ModItems.N_HAMMER);
+            content.add(ModItems.RUBY_KATANA);
+            content.add(ModItems.COPPER_C_AXE);
+        });
 
-    public static void registerItemGroups() {
-        Registry.register(Registries.ITEM_GROUP, new Identifier("items","skbitems"), SKBITEMS);
-        Registry.register(Registries.ITEM_GROUP, new Identifier("combat","scombat"), SKCOMABT);
-        Registry.register(Registries.ITEM_GROUP, new Identifier("fun","sfun"), SFUN);
-        Registry.register(Registries.ITEM_GROUP, new Identifier("utils","sutils"), SUTILS);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> {
+            content.addAfter(Items.GOLD_NUGGET ,ModItems.RUBY);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(content ->{
+            content.addAfter(Items.NETHERITE_HOE, ModItems.CHOPPY);
+            content.addBefore(Items.WOODEN_SHOVEL, ModItems.IA_SHOVEL);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.OPERATOR).register(content -> {
+            content.add(ModItems.OP_HAMMER);
+        });
     }
 }
