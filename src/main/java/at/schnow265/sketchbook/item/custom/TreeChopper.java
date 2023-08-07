8839ab231107f53adc616f0200f32f0722ee8432
schnow265 <tamschnow265@proton.me>
2023-08-07 20:01:54 +0200

    Try adding Enchanting Lore. It's a good mod to work with

--- src/main/java/at/schnow265/sketchbook/item/custom/TreeChopper.java
+++ src/main/java/at/schnow265/sketchbook/item/custom/TreeChopper.java
@@ -2,6 +2,7 @@ package at.schnow265.sketchbook.item.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.AxeItem;
@@ -14,6 +15,8 @@ import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 import java.util.List;
 
@@ -26,13 +29,16 @@ public class TreeChopper extends AxeItem {
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
         PlayerEntity player = context.getPlayer();
-        if (player != null && !player.isSneaking()) {
+        if (player != null) {
+            boolean isShiftHeld = InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), GLFW.GLFW_KEY_LEFT_SHIFT) || InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), GLFW.GLFW_KEY_RIGHT_SHIFT);
+
             Block block = context.getWorld().getBlockState(context.getBlockPos()).getBlock();
             if (block.equals(Blocks.OAK_LOG) || block.equals(Blocks.SPRUCE_LOG) || block.equals(Blocks.BIRCH_LOG) ||
                     block.equals(Blocks.JUNGLE_LOG) || block.equals(Blocks.ACACIA_LOG) || block.equals(Blocks.DARK_OAK_LOG) || block.equals(Blocks.MANGROVE_LOG)) {
                 // The block is a log block
                 context.getWorld().breakBlock(context.getBlockPos(), true, player);
-                int radius = 5; // the radius of the tree
+                int radius = isShiftHeld ? 100 : 10; // Change the radius based on whether Shift is held or not
+
                 for (int x = -radius; x <= radius; x++) {
                     for (int y = -radius; y <= radius; y++) {
                         for (int z = -radius; z <= radius; z++) {
@@ -51,9 +57,9 @@ public class TreeChopper extends AxeItem {
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
         if(Screen.hasShiftDown()) {
-            tooltip.add(Text.literal("Left click on a tree for some chopping!").formatted(Formatting.AQUA));
+            tooltip.add(Text.translatable("tooltips.choppy.shift").formatted(Formatting.RED));
         } else {
-            tooltip.add(Text.literal("Press Shift for more info!").formatted(Formatting.YELLOW));
+            tooltip.add(Text.translatable("tooltips.choppy.nshift").formatted(Formatting.WHITE));
         }
 
         super.appendTooltip(stack, world, tooltip, context);
